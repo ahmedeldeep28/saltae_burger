2e46eacf4abc2506ebed159f5a21ea6499f62711
ahmed eldeep <a7medeldeep28@gmail.com>
2022-12-03 13:15:13 +0200

    fix error production

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 // import css style 
 import './custom.scss';
 import './App.scss';
